6a4eb6498a5161ebd221ad874ad1cae237b8737a
Kolton Underberg <Kolton.Underberg@vu.com>
2023-07-07 10:23:36 -0500

    r

--- classy_person.py
+++ classy_person.py
@@ -8,6 +8,31 @@ don't forget the use of 'self' and to have the methods:
 '''
 
 class Person:
+    age = 0
+
+    object_name =""
+
+    def __init__(self, age, object_name):
+        self.age = age
+        self.object_name = object_name
+    
+    def increase_age(self):
+        self.age = self.age + 1
+        print(self.age)
+
+    def say_greeting(self):
+        object_name = self.object_name
+        print(f"Hello World! My name is {object_name}!")
+    
+    def count_to_age(self):
+        for a in range(0,self.age):
+            print(a)
+
+x = Person(20, "Lonny")
+
+x.increase_age()
+x.say_greeting()
+x.count_to_age()
 
 
 
